db786a1783462adc05ba7ef9ce692bce5fde8245
Anurag Kyal <anurag.kyal@snapchat.com>
2017-11-16 18:03:03 -0800

    Adding line and some buttons

---
+++ app/src/main/java/com/example/anuragkyal/autograph/ui/DrawingView.java
@@ -0,0 +1,130 @@
+package com.example.anuragkyal.autograph.ui;
+
+import static android.support.v4.content.ContextCompat.getColor;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import com.example.anuragkyal.autograph.R;
+
+
+public class DrawingView extends View {
+
+    Context mContext;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint mBitmapPaint;
+    private Paint mStrokePaint;
+    private Paint mCirclePaint;
+    private Path mCirclePath;
+
+    public DrawingView(Context context) {
+        this(context, null /*attributeSet*/);
+    }
+
+    public DrawingView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        mContext =context;
+        mPath = new Path();
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+
+        mCirclePaint = new Paint();
+        mCirclePath = new Path();
+        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setColor(Color.BLUE);
+        mCirclePaint.setStyle(Paint.Style.STROKE);
+        mCirclePaint.setStrokeJoin(Paint.Join.MITER);
+        mCirclePaint.setStrokeWidth(4f);
+
+        mStrokePaint = new Paint();
+        mStrokePaint.setAntiAlias(true);
+        mStrokePaint.setDither(true);
+        mStrokePaint.setColor(getColor(mContext, R.color.signature_stroke));
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setStrokeJoin(Paint.Join.ROUND);
+        mStrokePaint.setStrokeCap(Paint.Cap.ROUND);
+        mStrokePaint.setStrokeWidth(12);
+    }
+
+    // how does the initial size become full screen?
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(getColor(mContext, R.color.signature_background_default));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
+        canvas.drawPath( mPath, mStrokePaint);
+        canvas.drawPath(mCirclePath, mCirclePaint);
+    }
+
+    private float mX, mY;
+    private static final float TOUCH_TOLERANCE = 4;
+
+    private void touchStart(float x, float y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            // what is quadTo
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+
+            mCirclePath.reset();
+            mCirclePath.addCircle(mX, mY, 30, Path.Direction.CW);
+        }
+    }
+
+    private void touchUp() {
+        mPath.lineTo(mX, mY);
+        mCirclePath.reset();
+        // commit the path to our offscreen
+        mCanvas.drawPath(mPath, mStrokePaint);
+        // kill this so we don't double draw
+        mPath.reset();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+}
