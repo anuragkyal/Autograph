ac4b4dadc1fc7b5a8fde1d02091de46bfc8361bd
Anurag Kyal <anurag.kyal@snapchat.com>
2017-11-17 12:57:07 -0800

    Adding timber for logging

--- app/src/main/java/com/example/anuragkyal/autograph/MainActivity.java
+++ app/src/main/java/com/example/anuragkyal/autograph/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.example.anuragkyal.autograph.ui.DrawingView;
 
 public class MainActivity extends AppCompatActivity {
